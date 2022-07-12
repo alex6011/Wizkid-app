0bcd6774f941c539eb8a051810bf641fa96739c9
Aleksandar <alexanderstefanov438@gmail.com>
2022-07-12 16:15:56 +0200

    Route guards
    If not logged in, accessing certain routes when you type the URL is not possible anymore. Only people with proper authentication can do that.

--- test/src/app/app-routing.module.ts
+++ test/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
 import { BaseComponent } from './base/base.component';
 import { UserOverviewComponent } from './components/user-overview/user-overview.component';
 import { LoginComponent } from './containers/login/login.component';
@@ -8,8 +9,16 @@ import { NewWizkidComponent } from './containers/new-wizkid/new-wizkid.component
 const routes: Routes = [
   { path: '', redirectTo: '/base', pathMatch: 'full' },
   { path: 'base', component: BaseComponent },
-  { path: 'user/:id', component: UserOverviewComponent },
-  { path: 'newWizkid', component: NewWizkidComponent },
+  {
+    path: 'user/:id',
+    component: UserOverviewComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'newWizkid',
+    component: NewWizkidComponent,
+    canActivate: [AuthGuard],
+  },
   { path: 'login', component: LoginComponent },
 ];
 
---
+++ test/src/app/auth.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { WizkidsService } from './services/wizkids.service';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
+  constructor(private wizkidsService: WizkidsService, private router: Router) {}
+
+  async canActivate(): Promise<boolean> {
+    const authenticated = await this.wizkidsService.isLoggedIn();
+    if (authenticated) {
+      return true;
+    } else {
+        this.router.navigate(['/base']);
+        return false;
+      //   window.location.href = "/base";
+     
+    }
+  }
+}
